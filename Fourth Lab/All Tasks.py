5a7189ef3ae00c8103c34ea1c1036eb4df54046e
Meteoroetem <rotem.kassif@gmail.com>
2023-02-20 14:42:21 +0200

    Finished Fourth Lab

--- Fourth Lab/All Tasks.py
+++ Fourth Lab/All Tasks.py
@@ -1,2 +1,19 @@
 def FirstTask(num):
-    for i in range(num)
+    cohenCnt = 0
+    for i in range(num):
+        inp = str(input("What's student #"+str(i+1)+"'s name?"))
+        cohenCnt += inp.count("cohen")
+    print(cohenCnt)
+
+def SecondTask(string):
+    strArr = str(string).split(' ')
+    print(strArr[0],strArr[len(strArr)-1])
+
+def ThirdTask(string):
+    print(string[0] + string[len(string)-1])
+
+def FourthTask(string):
+    strArr = str(string).split(' ')
+    for word in strArr:
+        print(word[0], end="")
+
